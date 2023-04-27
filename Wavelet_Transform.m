d0e7a9d0678215f1b89eb36fdc647786dfafc233
optimus5383 <94214299+optimus5383@users.noreply.github.com>
2023-04-27 17:27:43 +0900

    Revert "Prequel"
    This reverts commit 6da0c5e1433779f37a7549be0ba3326368c8901c.

--- Wavelet_Transform.m
+++ Wavelet_Transform.m
@@ -8,53 +8,46 @@ Select_variable='Test';
 File_List=dir(File_PATH);
 [Data_num,~] = size(File_List);
 
-VPO=5;
-labels=zeros(Data_num-2,6)
+VOP=5;
+
 % File Selection Section
-for i = 3: Data_num+1
-    file_name=File_List(i).name;
-    name_parts=split(file_name,'_')
-    file_name=strcat(File_PATH,'/',file_name);
-    data = readtable(file_name);
-    % Data parsing
-    
-    data = renamevars(data, "TIME", "Time");
-    t = data.Time;
-    Vgs = data.Vgs;
-    Vds = data.Vds;
-    Id = data.Id;
-    
-    % Perform Contimuous Wavelet Transform
-    
-    fs = length(t)/(t(length(t)) - t(1));
-    [cfs_Id, f_Id] = cwt(Id, 'Morse', fs ,VoicePerOctave=VPO);
-    [cfs_Vds, f_Vds] = cwt(Vds, 'Morse', fs ,VoicePerOctave=VPO);
-    [cfs_Vgs, f_Vgs] = cwt(Vgs, 'Morse', fs ,VoicePerOctave=VPO);
-    
-    % Resize Data
-    
-    original_num_columns = length(cfs_Id);
-    
-    num_columns_resampled = original_num_columns/50; % New number of columns
-    resample_factor = size(cfs_Id, 2) / num_columns_resampled;
-    t_res = resample(t.', 1, resample_factor).';
-    
-    % ID
-    cfs_Id_res = resample(cfs_Id.', 1, resample_factor).';
-    
-    % VDS
-    cfs_Vds_res = resample(cfs_Vds.', 1, resample_factor).';
-    
-    % VGS
-    cfs_Vgs_res = resample(cfs_Vgs.', 1, resample_factor).';
 
-    % On Resistancs, Off resistances, Pulse time , Vds, Vgs_on , Vgs_of,
-    % Reduce ratio
+file_name=File_List(3).name;
+file_name=strcat(File_PATH,'/',file_name);
+data = readtable(file_name);
 
-    labels(i-1,:)=[5,5,str2double(name_parts{2}),str2double(name_parts{4}),...
-        0.1*str2double(name_parts{6}),0.1*str2double(name_parts{8}),resample_factor];
+% Data parsing
 
-end
+data = renamevars(data, "TIME", "Time");
+t = data.Time;
+Vgs = data.Vgs;
+Vds = data.Vds;
+Id = data.Id;
+
+% Perform Contimuous Wavelet Transform
+
+fs = length(t)/(t(length(t)) - t(1));
+[cfs_Id, f_Id] = cwt(Id, 'Morse', fs ,VoicePerOctave=VOP);
+[cfs_Vds, f_Vds] = cwt(Vds, 'Morse', fs ,VoicePerOctave=VOP);
+[cfs_Vgs, f_Vgs] = cwt(Vgs, 'Morse', fs ,VoicePerOctave=VOP);
+
+% Resize Data
+
+original_num_columns = length(cfs_Id);
+
+num_columns_resampled = original_num_columns/50; % New number of columns
+resample_factor = size(cfs_Id, 2) / num_columns_resampled;
+t_res = resample(t.', 1, resample_factor).';
+
+% ID
+cfs_Id_res = resample(cfs_Id.', 1, resample_factor).';
+
+% VDS
+cfs_Vds_res = resample(cfs_Vds.', 1, resample_factor).';
+
+% VGS
+cfs_Vgs_res = resample(cfs_Vgs.', 1, resample_factor).';
+
 
 
 % % Concat The data and reshpe [3 * 174 * length]
